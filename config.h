1092983bd18ea3255bcbd61e9668b0d0388cc4b9
ross marks <rossmarks@proton.me>
2023-08-14 00:11:21 +0300

    updated key bindings

--- config.h
+++ config.h
@@ -77,7 +77,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
+	/* { MODKEY,                       XK_Tab,    view,           {0} }, */
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
@@ -94,8 +94,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-    { MODKEY,                       XK_i,      shiftview,      { .i = +1 } },
-	{ MODKEY,                       XK_u,      shiftview,      { .i = -1 } },
+    { MODKEY,                       XK_Tab,    shiftview,      { .i = +1 } },
+	{ MODKEY|ShiftMask,             XK_Tab,    shiftview,      { .i = -1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
